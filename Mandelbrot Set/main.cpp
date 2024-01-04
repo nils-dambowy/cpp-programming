5ec296cac79a1bf3232b05628cecbeb34a048f06
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-01-04 17:33:56 +0100

    Update main.cpp

--- Mandelbrot Set/main.cpp
+++ Mandelbrot Set/main.cpp
@@ -2,13 +2,11 @@
 #include <iostream>
 #include <SDL.h>
 
-using namespace std;
-
 struct color {
-	float r, g, b, a;
+	float r, g, b;
 };
 
-int mandel(complex<double> z, complex<double> c) {
+int mandel(std::complex<double> z, std::complex<double> c) {
 	int n = 0.0;
 	while (n < 255) {
 		z = z * z + c;
@@ -19,17 +17,12 @@ int mandel(complex<double> z, complex<double> c) {
 	return n;
 }
 
-int main(int argc, char* argv[]) {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		std::cout << "Unable to initialize SDL: " << SDL_GetError();
-		return 1;
-	}
-	
+int main(int argc, char* argv[])
 	// Color palette
-	color palette[] = { {0,0,0}, {0,24,32}, {26,61,74}, {79,90,100}, {147,167,184}};
-
+	//color palette[] = { {0,0,0}, {0,24,32}, {26,61,74}, {79,90,100}, {147,167,184}};
+	color palette[] = { { 0, 31, 63 }, { 0, 64, 128 }, { 0, 116, 228 }, { 0, 168, 232 }, { 0, 150, 136 }, { 76, 175, 80 }, { 131, 56, 236 }, { 199, 125, 255 }, { 233, 30, 99 }, { 255, 0, 184 }, { 255, 87, 51 }, { 255, 215, 0 } };
 	// Create a window
-	const int HEIGHT = 720;
+	const int HEIGHT = 720; 
 	const int WIDTH = 1280;
 	SDL_Window *window;
 
@@ -42,12 +35,12 @@ int main(int argc, char* argv[]) {
 
 	for (int x = 0; x < WIDTH; ++x) {
 		for (int y = 0; y < HEIGHT; ++y) {
-			complex<double> c(1.0* (x - (WIDTH - HEIGHT) / 2) / HEIGHT * 4.0 - 2.0, 1.0 * y / HEIGHT * 4.0 - 2.0);
-			complex<double> z(0.0, 0.0);
+			std::complex<double> c(1.0* (x - (WIDTH - HEIGHT) / 2) / HEIGHT * 4.0 - 2.0, 1.0 * y / HEIGHT * 4.0 - 2.0);
+			std::complex<double> z(0.0, 0.0);
 			int m = mandel(z, c);
 
 			// choose color based on iteration count when breaking
-			color random_color = palette[m % 5];
+			color random_color = palette[m % 12];
 		
 			SDL_SetRenderDrawColor(renderer, random_color.r, random_color.g, random_color.b, 255);
 			SDL_RenderDrawPoint(renderer,x, y);
